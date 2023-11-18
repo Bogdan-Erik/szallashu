0746603e639a7d512cdf4d7228017419c1bf14ba
Erik Bogdan <erik.bogdan@gmail.com>
2023-11-18 19:02:31 +0100

    Initialized Company structure, added excel import for migration

---
+++ app/Imports/CountriesImport.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Imports;
+
+use App\Models\Company;
+use Illuminate\Support\Facades\Hash;
+use Maatwebsite\Excel\Concerns\ToModel;
+use Maatwebsite\Excel\Concerns\WithHeadingRow;
+use Maatwebsite\Excel\Concerns\WithStartRow;
+use Maatwebsite\Excel\Concerns\ToCollection;
+use Illuminate\Support\Collection;
+
+use Maatwebsite\Excel\Concerns\WithCustomCsvSettings;
+
+class CountriesImport implements ToCollection, WithHeadingRow, WithStartRow
+{
+
+    public function collection(Collection $rows)
+    {
+        foreach ($rows as $row) {
+            Company::create([
+                'company_id' => $row['companyid'],
+                'company_name' => $row['companyname'],
+                'company_registration_number' => $row['companyregistrationnumber'],
+                'company_foundation_date' => $row['companyfoundationdate'],
+                'country' => $row['country'],
+                'zip_code' => $row['zipcode'],
+                'city' => $row['city'],
+                'street_address' => $row['streetaddress'],
+                'latitude' => $row['latitude'],
+                'longitude' => $row['longitude'],
+                'company_owner' => $row['companyowner'],
+                'employees' => $row['employees'],
+                'activity' => $row['activity'],
+                'active' => $row['active'],
+                'email' => $row['email'],
+                'password' => Hash::make($row['password']),
+            ]);
+        }
+    }
+
+    public function startRow(): int
+    {
+        return 2;
+    }
+}
---
+++ app/Models/Company.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Company extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'company_id',
+        'company_name',
+        'company_registration_number',
+        'company_foundation_date',
+        'country',
+        'zip_code',
+        'city',
+        'street_address',
+        'latitude',
+        'longitude',
+        'company_owner',
+        'employees',
+        'activity',
+        'active',
+        'email',
+        'password',
+    ];
+}
